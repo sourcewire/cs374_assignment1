04129401cfade22301b83beebed0f20acee52cd0
sourcewire <cyberskis@proton.me>
2023-10-10 23:53:46 -0700

    starting assignment 1

---
+++ README.txt
@@ -0,0 +1,2 @@
+compile with gcc --std=gnu99 -o [executable name] main.c
+./[exectuable name] file_name.csv
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* struct for movie info*/
+struct movie
+{
+    char* title;
+    int year;
+    char* languages;
+    float rating_value;
+}
